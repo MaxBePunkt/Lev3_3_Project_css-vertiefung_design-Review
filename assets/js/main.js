be739bc169631d33f611014be561b70eb559ccc8
MaxBePunkt <maximillianborret@gmail.com>
2021-11-03 15:27:01 +0100

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
 // console.log("test");
+
+function formChange() {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let message = document.getElementById("message").value;
+
+  console.log(name);
+  console.log(email);
+  console.log(message);
+}
